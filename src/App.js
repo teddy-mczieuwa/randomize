408dc89b1b862ad36f40b81276c57b6e77c0000b
Teddy <teddymczieuwa@gmail.com>
2021-04-06 17:22:19 +0100

    set up a button that generates a random number and sets it as the name for the button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
+
 function App() {
+  const [randomCount, setRandomCount] = useState(0)
+  const updateNumber = () => {
+    setRandomCount(Math.floor(Math.random() * 100))
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="btn" onClick={updateNumber}>{randomCount}</button>
       </header>
     </div>
   );
